8aa0d784f0c7db0a7d7111abdbce3098f7d1176d
vaultec <47548474+vaultec81@users.noreply.github.com>
2023-01-24 18:31:28 -0800

    fix: add consensusRoundLength to networks.ts

--- src/services/networks.ts
+++ src/services/networks.ts
@@ -1,7 +1,10 @@
 export default {
     "testnet/ab8b6cf1-b344-4ad3-8f81-f2d72c61f6b2": {
         genesisDay: 70790692,
-        roundLength: 20
+        roundLength: 20,
+        //10 Rounds per every consensus period. 
+        //Thus 20 blocks per round * 3 seconds = 60 * 10 = 600 seconds = 10 minutes
+        consensusRoundLength: 10, 
     }
 } as Record<string, {
     genesisDay: number,

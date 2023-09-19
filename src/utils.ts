5887b8a2358fe88239d21f436f504d4b2899c5fb
vaultec <47548474+vaultec81@users.noreply.github.com>
2023-09-19 09:58:40 -0700

    feat: real time block streaming using new system

--- src/utils.ts
+++ src/utils.ts
@@ -137,37 +137,47 @@ export class fastStream {
     this.logger.debug("ITS IDLE", {
       finalBlock
     })
+
+    try {
+      for await(let block of liveHiveBlocks(HIVE_API, {
+        start_block: this.parser_height,
+        stop: () => {}
+      })) {
+        const block_height = parseInt(block.block_id.slice(0, 8), 16)
+        if (this.parser_height === block_height) {
+          this.parser_height = block_height + 1;
+          this.currentBlock = block_height;
+          this.events.emit('block', block_height, block)
+        } else if(block_height > this.parser_height) {
+          this.blockMap[block_height] = block
+        }
+      }
+    } catch(error) {
+      clearInterval(this.eventQueue as any)
+      this.streamOut.end(error)
+    }
+
     
-    this.finalStream = HiveClient.blockchain.getBlockStream({
-        from: this.parser_height,
-        mode: BlockchainMode.Latest
-    })
-    await new Promise((resolve) => {
-      this.finalStream
-        .on('data', (async function (block) {
-          const block_height = parseInt(block.block_id.slice(0, 8), 16)
-          if (this.parser_height === block_height) {
-            this.parser_height = block_height + 1;
-            this.currentBlock = block_height;
-            this.events.emit('block', block_height, block)
-          } else if(block_height > this.parser_height) {
-            this.blockMap[block_height] = block
-          }
-        }).bind(this))
-        .on('error', ((error) => {
-          clearInterval(this.eventQueue as any)
-          this.streamOut.end(error)
-        }).bind(this))
-        .on('end', (function () {
-          // done
-          activeLength = activeLength - 1
-          if (activeLength === 0) {
-            //events.emit('end')
-          }
-          this.finalStream.removeAllListeners()
-          return resolve(null)
-        }).bind(this))
-    })
+    // this.finalStream = HiveClient.blockchain.getBlockStream({
+    //     from: this.parser_height,
+    //     mode: BlockchainMode.Latest
+    // })
+    // await new Promise((resolve) => {
+    //   this.finalStream
+    //     .on('data', (async function (block) {
+    //     }).bind(this))
+    //     .on('error', ((error) => {
+    //     }).bind(this))
+    //     .on('end', (function () {
+    //       // done
+    //       activeLength = activeLength - 1
+    //       if (activeLength === 0) {
+    //         //events.emit('end')
+    //       }
+    //       this.finalStream.removeAllListeners()
+    //       return resolve(null)
+    //     }).bind(this))
+    // })
   }
 
   async resumeStream() {
@@ -207,6 +217,99 @@ export class fastStream {
   }
 }
 
+function parseHiveBlocks(blocksInput) {
+  const blocks = blocksInput.map(block => {
+    block.transactions = block.transactions.map((tx, index) => {
+      tx.operations = tx.operations.map(op => {
+        const typeS = op.type.split('_')
+        const typeB = typeS.splice(0, typeS.length - 1).join('_');
+        if(typeB === 'transfer') {
+          let unit;
+          if(op.value.amount.nai === '@@000000021') {
+            unit = 'HIVE'
+          } else if (op.value.amount.nai === '@@000000013') {
+            unit = 'HBD'
+          }
+          op.value.amount = `${Number(op.value.amount.amount) / Math.pow(10, op.value.amount.precision)} ${unit}`;
+        }
+        return [typeS.splice(0, typeS.length - 1).join('_'), op.value]
+      })
+      tx.transaction_id = block.transaction_ids[index]
+      tx.block_num = parseInt(block.block_id.slice(0, 8), 16)
+      tx.timestamp = block.timestamp
+      return tx;
+    })
+    return block
+  })
+  return blocks
+}
+
+export async function* liveHiveBlocks(API, opts: {
+  start_block: number,
+  stop: Function
+}) {
+
+  let last_block = opts.start_block
+  let count = 0;
+  let bh = 0;
+  setInterval(async() => {
+    const hive = new Client(HIVE_API)
+    bh = await hive.blockchain.getCurrentBlockNum()
+    const dynProps = await hive.database.getDynamicGlobalProperties()
+    // console.log({
+    //   last_irreversible_block_num: dynProps.last_irreversible_block_num,
+    //   head_block_number: dynProps.head_block_number
+    // })
+    // console.log(bh, last_block,)
+    count = bh - last_block 
+    // console.log('count', count)
+  }, 500)
+
+
+  for( ; ; ) {
+    if(count < 1 ) {
+      await sleep(50)
+      continue;
+    }
+    const requestTime = new Date();
+    
+    // console.log(parseHiveBlocks(data.result.blocks).map(e => {
+      //   console.log(e)
+      //   return parseInt(e.block_id.slice(0, 8), 16)
+      // }), bh)
+      if(bh > last_block + 1) {
+      const {data} = await Axios.post(API, {
+        "jsonrpc":"2.0", 
+        "method":"block_api.get_block_range", 
+        "params":{
+          "starting_block_num": last_block, 
+          "count": count
+        }, 
+        "id":1
+      })
+      
+      for(let block of parseHiveBlocks(data.result.blocks)) {
+        console.log('281', last_block, parseInt(block.block_id.slice(0, 8), 16), bh)
+        last_block = parseInt(block.block_id.slice(0, 8), 16)
+        count = bh - last_block
+        console.log(new Date().getTime() - new Date(block.timestamp + "Z").getTime())
+        // console.log(block.timestamp, new Date().toISOString(), new Date().getTime() - new Date(block.timestamp + "Z").getTime(), new Date().getTime() - requestTime.getTime())
+        yield block
+      }
+    }
+    if(bh === last_block + 1) {
+      const lstBlock = await HiveClient.database.getBlock(bh)
+      last_block = parseInt(lstBlock.block_id.slice(0, 8), 16)
+      // console.log('300', last_block, parseInt(lstBlock.block_id.slice(0, 8), 16), bh)
+      // console.log(lstBlock)
+      // console.log(new Date().getTime() - new Date(lstBlock.timestamp + "Z").getTime())
+      count = bh - last_block
+      yield lstBlock
+    }
+  }
+
+}
+
 export async function streamHiveBlocks(API, opts) {
   for(let attempt = 0; attempt < 5; attempt++) {
     try {
@@ -220,20 +323,7 @@ export async function streamHiveBlocks(API, opts) {
         "id":1
       })
       
-      const blocks = data.result.blocks.map(block => {
-        block.transactions = block.transactions.map((tx, index) => {
-          tx.operations = tx.operations.map(op => {
-            const typeS = op.type.split('_')
-            return [typeS.splice(0, typeS.length - 1).join('_'), op.value]
-          })
-          tx.transaction_id = block.transaction_ids[index]
-          tx.block_num = parseInt(block.block_id.slice(0, 8), 16)
-          tx.timestamp = block.timestamp
-          return tx;
-        })
-        return block
-      })
-      return blocks
+      return parseHiveBlocks(data.result.blocks)
     } catch (ex) {
       console.log(ex)
       if(attempt === 0) {

971beed34372fa8b0803e038972ee0babc0e6a47
vaultec <47548474+vaultec81@users.noreply.github.com>
2023-12-24 06:46:49 -0800

    tweak: vm-runner, vm-runner test support latest runtime methods

--- src/services/new/vm/vm-runner.test.ts
+++ src/services/new/vm/vm-runner.test.ts
@@ -6,11 +6,13 @@ import asc from "assemblyscript/dist/asc";
 import * as IPFS from 'kubo-rpc-client'
 import {fork} from 'child_process'
 import Crypto from 'crypto'
+import { VmContainer } from './utils';
 
 
 const ipfs = IPFS.create({url: 'http://127.0.0.1:5001'})
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 
 
 void (async () => {
@@ -55,101 +57,62 @@ void (async () => {
     const cid = await ipfs.block.put(binary)
     console.log(cid)
 
+    const vmContainer = new VmContainer({
+      contract_id: 'vs41q9c3yg8estwk8q9yjrsu2hk6chgk5aelwlf8uj3amqfgywge8w3cul438q9tx556',
+      cid: cid.toString()
+    })
 
-    const parameters = [];
-    const options = {
-    stdio: [ 'pipe', 'pipe', 'pipe', 'ipc' ]
-    };
-    
-    const partPath = path.join(__dirname, 'vm-runner.js').replace('src', 'dist')
+    await vmContainer.init()
+    await vmContainer.onReady()
 
-    const child = fork(partPath, parameters, {
-        env: {
-            cid: cid.toString()
-        } as any,
-        detached: false
-    });
-    const pid = child.pid
-    console.log(pid)
-    child.send({
+
+    for(let x = 0; x < 1; x++) {
+      const result = await vmContainer.call({
+        action: 'testJSON',
+        payload: JSON.stringify({
+          to: "test1",
+          from: 'test2',
+          
+        })
+      })
+      console.log(result)
+    }    
         
-    });
+
+    await vmContainer.finish()
+
     let reqId;
     let startTime
-    child.on('message', (message: any) => {
-        if(message.type === "ready") {
-            reqId = Crypto.randomBytes(6).toString('base64url')
-            startTime = new Date();
-            console.log('startTime', startTime)
-            const payload = JSON.stringify({
-                to: "test1",
-                from: 'test2',
-            })
-            child.send({
-                type: "call",
-                action: "testJSON",
-                payload,
-                reqId
-            });
-        }
-        if(message.reqId === reqId) {
-            console.log(message, new Date().getTime() - startTime.getTime(), new Date())
-            reqId = Crypto.randomBytes(6).toString('base64url')
-            startTime = new Date();
-            const payload = JSON.stringify({
-              to: "test1",
-              from: 'test2',
-            })
-            child.send({
-              type: "call",
-              action: "testJSON",
-              payload,
-              reqId
-          });
-        }
-    });
-
-    // if(binary) {
-    //     let dataStore = new Map()
-    //     const insta = await instantiate(binary, {
-    //         input: {
-    //             consoleLog: (d) => {
-                    
-    //                 console.log('d', insta.exports.__getString(d))
-    //                 // return 44
-    //             },
-    //             logNumber: (d) => {
-    //                 console.log('logNumber', d)
-    //             },
-    //             logBool: (d) => {
-    //                 console.log('logBool', d)
-    //             },
-    //             base64: () => {
-    //               return base64
-    //             },
-    //             logUint8Array: (d) => {
-    //                 console.log('logUint8Array', d)
-    //             },
-    //             "db.setObject": (keyPtr, valPtr) => {
-    //                 const key = insta.exports.__getString(keyPtr)
-    //                 const val = insta.exports.__getString(valPtr)
-                    
-    //                 console.log('setObject', key, val)
-    //                 dataStore.set(key, val)
-    //                 return 1;
-    //             },
-    //             "db.getObject": (key) => {
-    //                 console.log('getObject', key)
-    //                 return "hello"
-    //             },
-    //             api: async () => {
-    //                 const { data } = await Axios.get('http://ipinfo.io/json')
-    //                 console.log(data)
-                    
-    //                 return insta.exports.__newString(data.ip)
-    //             }
-    //         }
-    //     } as any)
-
-    // }
+    // child.on('message', (message: any) => {
+    //     if(message.type === "ready") {
+    //         reqId = Crypto.randomBytes(6).toString('base64url')
+    //         startTime = new Date();
+    //         console.log('startTime', startTime)
+    //         const payload = JSON.stringify({
+    //             to: "test1",
+    //             from: 'test2',
+    //         })
+    //         child.send({
+    //             type: "call",
+    //             action: "testJSON",
+    //             payload,
+    //             reqId
+    //         });
+    //     }
+    //     if(message.reqId === reqId) {
+    //         console.log(message, new Date().getTime() - startTime.getTime(), new Date())
+    //         reqId = Crypto.randomBytes(6).toString('base64url')
+    //         startTime = new Date();
+    //       //   const payload = JSON.stringify({
+    //       //     to: "test1",
+    //       //     from: 'test2',
+    //       //   })
+    //       //   child.send({
+    //       //     type: "call",
+    //       //     action: "testJSON",
+    //       //     payload,
+    //       //     reqId
+    //       // });
+    //     }
+    // });
 })()
--- src/services/new/vm/vm-runner.ts
+++ src/services/new/vm/vm-runner.ts
@@ -1,319 +1,330 @@
 import ivm from 'isolated-vm'
 import * as IPFS from 'kubo-rpc-client'
 import Axios from 'axios'
+import metering from 'wasm-metering'
 import { addLink } from '../../../ipfs-utils/add-link'
 import { removeLink } from '../../../ipfs-utils/rm-link'
 import { instantiate } from './utils'
 const CID = IPFS.CID
 
-const ipfs = IPFS.create({url: 'http://127.0.0.1:5001'})
-
-class RunnerBase {
-    async set() {
-
-    }
-    async get() {
-
-    }
-    async ls() {
-
-    }
-}
+const ipfs = IPFS.create({ url: 'http://127.0.0.1:5001' })
 
 
 export class WasmRunner {
-    
-    constructor() {
+  stateCache: Map<string, any>
+  constructor() {
+    this.stateCache = new Map()
+  }
 
+  async contractStateRaw(id: string, stateMerkle?: string) {
+    let stateCid
+    // let contract = await this.contractDb.findOne({
+    //   id,
+    // })
+    const contract = {} as any
+    if (!contract) {
+      throw new Error('Contract Not Indexed Or Does Not Exist')
     }
-
-    async contractStateRaw(id: string, stateMerkle?: string) {
-        let stateCid
-        // let contract = await this.contractDb.findOne({
-        //   id,
-        // })
-        const contract = {
-            
-        } as any
-        if(!contract) {
-          throw new Error("Contract Not Indexed Or Does Not Exist")
-        }
-        if (contract) {
-          if (stateMerkle) {
-            stateCid = CID.parse(stateMerkle)
-          } else {
-            if (contract.state_merkle) {
-              stateCid = CID.parse(contract.state_merkle)
-            } else {
-              stateCid = await ipfs.object.new()
-            }
-          }
-        }
-    
-        return {
-          client: {
-            /**
-             * 
-             * @param id Contract ID
-             */
-            remoteState: async(id: string) => {
-              const state = await this.contractStateRaw(id)
-    
-              return {
-                pull: state.client.pull,
-                ls: state.client.ls,
-              }
-            },
-            pull: async (key: string) => {
-              try {
-                console.log(stateCid)
-                const out = await ipfs.dag.resolve(stateCid, {
-                  path: key,
-                })
-    
-                const data = await ipfs.dag.get(out.cid)
-                
-                console.log(data)
-                if (out.cid.code === 0x70) {
-                  //If accidentally requesting PD-dag
-                  const out = await ipfs.dag.resolve(stateCid, {
-                    path: `${key}/.self`,
-                  })
-      
-                  const data = await ipfs.dag.get(out.cid)
-                  return data.value;
-                } else if (out.cid.code === 0x71) {
-                  //CBOR dag
-                  return data.value
-                } else {
-                  //This shouldn't happen unless other issues are present.
-                  return null
-                }
-              } catch (ex) {
-                console.log(ex)
-                return null
-              }
-            },
-            update: async (key, value: any) => {
-              try {
-    
-                if (!value) {
-                  return
-                }
-                let merkleCid = stateCid
-      
-                let linkExists
-                let dagData
-                let brokenPaths = []
-                try {
-                  const resolvedCid = await ipfs.dag.resolve(merkleCid, {
-                    path: key,
-                  })
-      
-                  const rawData = await ipfs.dag.get(resolvedCid.cid)
-                  if (resolvedCid.cid.code === 0x70) {
-                    dagData = JSON.parse(Buffer.from(rawData.value.Data).toString())
-                  } else if (resolvedCid.cid.code === 0x71) {
-                    dagData = rawData.value
-                  }
-                } catch (ex) {
-                  linkExists = false
-                  let splitKey = key.split('/')
-                  for (let x = 0; x < splitKey.length; x++) {
-                    let partialKey = splitKey.slice(0, splitKey.length - 1 - x)
-                    console.log(partialKey)
-      
-                    try {
-                      const cid = await ipfs.dag.resolve(merkleCid, {
-                        path: partialKey.join('/'),
-                      })
-                      console.log(cid.cid.code)
-                      if (cid.cid.code === 0x70) {
-                        break
-                      } else {
-                        brokenPaths.push({
-                          path: partialKey.join('/'),
-                          cid: cid.cid,
-                          wrongFormat: true,
-                        })
-                      }
-                    } catch {
-                      brokenPaths.push({ path: partialKey.join('/') })
-                    }
-                  }
-                }
-      
-                for (let brokenPath of brokenPaths.reverse()) {
-                  if (brokenPath.wrongFormat) {
-                    merkleCid = await ipfs.object.patch.addLink(merkleCid, {
-                      Name: brokenPath.path,
-                      Hash: CID.parse('QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'),
-                    })
-                    
-                    merkleCid = await ipfs.object.patch.addLink(merkleCid, {
-                      Name: `${brokenPath.path}/.self`,
-                      Hash: brokenPath.cid,
-                    })
-                  } else {
-                    merkleCid = await ipfs.object.patch.addLink(merkleCid, {
-                      Name: brokenPath.path,
-                      Hash: CID.parse('QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'),
-                    })
-                  }
-                }
-                
-                const dataCid = await ipfs.dag.put(value)
-                const stat = await ipfs.block.stat(dataCid)
-                
-                
-                merkleCid = (await addLink(ipfs, {
-                  parentCid: merkleCid,
-                  name: key,
-                  size: stat.size,
-          
-                  cid: dataCid,
-                  hashAlg: 'sha2-256',
-                  cidVersion: 1,
-                  flush: true,
-                  shardSplitThreshold: 2_000
-                })).cid
-    
-                stateCid = merkleCid
-              } catch (ex) {
-                console.log(ex)
-              }
-              //TODO make this happen after contract call has been completely executed
-              //stateCid = obj;
-            },
-            /**
-             * WIP: needs DAG type updates
-             * @param key 
-             * @returns 
-             */
-            ls: async (key) => {
-              try {
-                const pBufNode = await ipfs.object.get(`${stateCid}/${key}` as any)
-                console.log(pBufNode.Links)
-                return pBufNode.Links.map((e) => e.Name)
-              } catch {
-                return []
-              }
-            },
-            del: async (key) => {
-              if(!key) {
-                return;
-              }
-              let merkleCid = stateCid
-              //To be implemented!
-              merkleCid = await removeLink(ipfs, {
-                  parentCid: merkleCid,
-                  name: key,
-                  size: 0,
-          
-                  hashAlg: 'sha2-256',
-                  cidVersion: 1,
-                  flush: true,
-                  shardSplitThreshold: 2_000
-              })
-              stateCid = merkleCid
-            },
-          },
-          finish: () => {
-            return {
-              stateMerkle: stateCid,
-            }
-          },
-          startMerkle: stateCid,
+    if (contract) {
+      if (stateMerkle) {
+        stateCid = CID.parse(stateMerkle)
+      } else {
+        if (contract.state_merkle) {
+          stateCid = CID.parse(contract.state_merkle)
+        } else {
+          stateCid = CID.parse('QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn')
         }
       }
-
-    async initiate() {
-
     }
 
-    
-    
-    async testRun() {
-        
-    }
+    return {
+      client: {
+        /**
+         *
+         * @param id Contract ID
+         */
+        remoteState: async (id: string) => {
+          const state = await this.contractStateRaw(id)
 
-    registerBindings() {
+          return {
+            pull: state.client.pull,
+            ls: state.client.ls,
+          }
+        },
+        pull: async (key: string) => {
+          try {
+            console.log(stateCid)
+            const out = await ipfs.dag.resolve(stateCid, {
+              path: key,
+            })
 
+            const data = await ipfs.dag.get(out.cid)
+
+            console.log(data)
+            if (out.cid.code === 0x70) {
+              //If accidentally requesting PD-dag
+              const out = await ipfs.dag.resolve(stateCid, {
+                path: `${key}/.self`,
+              })
+
+              const data = await ipfs.dag.get(out.cid)
+              return data.value
+            } else if (out.cid.code === 0x71) {
+              //CBOR dag
+              return data.value
+            } else {
+              //This shouldn't happen unless other issues are present.
+              return null
+            }
+          } catch (ex) {
+            console.log(ex)
+            return null
+          }
+        },
+        update: async (key, value: any) => {
+          try {
+            if (!value) {
+              return
+            }
+            let merkleCid = stateCid
+
+            let linkExists
+            let dagData
+            let brokenPaths = []
+            try {
+              const resolvedCid = await ipfs.dag.resolve(merkleCid, {
+                path: key,
+              })
+
+              const rawData = await ipfs.dag.get(resolvedCid.cid)
+              if (resolvedCid.cid.code === 0x70) {
+                dagData = JSON.parse(Buffer.from(rawData.value.Data).toString())
+              } else if (resolvedCid.cid.code === 0x71) {
+                dagData = rawData.value
+              }
+            } catch (ex) {
+              linkExists = false
+              let splitKey = key.split('/')
+              for (let x = 0; x < splitKey.length; x++) {
+                let partialKey = splitKey.slice(0, splitKey.length - 1 - x)
+                console.log(partialKey)
+
+                try {
+                  const cid = await ipfs.dag.resolve(merkleCid, {
+                    path: partialKey.join('/'),
+                  })
+                  console.log(cid.cid.code)
+                  if (cid.cid.code === 0x70) {
+                    break
+                  } else {
+                    brokenPaths.push({
+                      path: partialKey.join('/'),
+                      cid: cid.cid,
+                      wrongFormat: true,
+                    })
+                  }
+                } catch {
+                  brokenPaths.push({ path: partialKey.join('/') })
+                }
+              }
+            }
+
+            for (let brokenPath of brokenPaths.reverse()) {
+              if (brokenPath.wrongFormat) {
+                merkleCid = await ipfs.object.patch.addLink(merkleCid, {
+                  Name: brokenPath.path,
+                  Hash: CID.parse('QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'),
+                })
+
+                merkleCid = await ipfs.object.patch.addLink(merkleCid, {
+                  Name: `${brokenPath.path}/.self`,
+                  Hash: brokenPath.cid,
+                })
+              } else {
+                merkleCid = await ipfs.object.patch.addLink(merkleCid, {
+                  Name: brokenPath.path,
+                  Hash: CID.parse('QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'),
+                })
+              }
+            }
+
+            const dataCid = await ipfs.dag.put(value)
+            const stat = await ipfs.block.stat(dataCid)
+
+            merkleCid = (
+              await addLink(ipfs, {
+                parentCid: merkleCid,
+                name: key,
+                size: stat.size,
+
+                cid: dataCid,
+                hashAlg: 'sha2-256',
+                cidVersion: 1,
+                flush: true,
+                shardSplitThreshold: 2_000,
+              })
+            ).cid
+
+            stateCid = merkleCid
+          } catch (ex) {
+            console.log(ex)
+          }
+          //TODO make this happen after contract call has been completely executed
+          //stateCid = obj;
+        },
+        /**
+         * WIP: needs DAG type updates
+         * @param key
+         * @returns
+         */
+        ls: async (key) => {
+          try {
+            const pBufNode = await ipfs.object.get(`${stateCid}/${key}` as any)
+            console.log(pBufNode.Links)
+            return pBufNode.Links.map((e) => e.Name)
+          } catch {
+            return []
+          }
+        },
+        del: async (key) => {
+          if (!key) {
+            return
+          }
+          let merkleCid = stateCid
+          //To be implemented!
+          merkleCid = await removeLink(ipfs, {
+            parentCid: merkleCid,
+            name: key,
+            size: 0,
+
+            hashAlg: 'sha2-256',
+            cidVersion: 1,
+            flush: true,
+            shardSplitThreshold: 2_000,
+          })
+          stateCid = merkleCid
+        },
+      },
+      finish: () => {
+        return {
+          stateMerkle: stateCid,
+        }
+      },
+      startMerkle: stateCid,
     }
+  }
+
+  async initiate() {}
+
+  async testRun() {}
+
+  registerBindings() {}
 }
 
-
 void (async () => {
-    
+  
+  const contract_id = process.env.contract_id
+  
+  const binaryData = await ipfs.block.get(IPFS.CID.parse(process.env.cid))
+  
+  const wasmRunner = new WasmRunner();
+  const stateAccess = await wasmRunner.contractStateRaw(contract_id)
+  
+  const module = await WebAssembly.compile(binaryData)
+  
+  process.send({
+    type: 'ready',
+  })
+  process.on('message', async (message: any) => {
+    if (message.type === 'call') {
+      const memory = new WebAssembly.Memory({
+        initial: 10,
+        maximum: 128,
+      })
+      
+      let IOGas = 0;
 
-    console.log(process.env.cid)
-    
-    const binaryData = await ipfs.block.get(IPFS.CID.parse(process.env.cid))
-    
-    console.log(binaryData)
+      const logs = []
+      const insta = await instantiate(module, {
+        env: {
+          memory,
+        },
+        input: {
+          'console.log': (keyPtr) => {
+            const logMsg = insta.exports.__getString(keyPtr)
+            logs.push(logMsg)
+            IOGas = IOGas + logMsg.length
+          },
+          'console.logNumber': (val) => {
+            logs.push(val)
+          },
+          'console.logBool': (val) => {
+            logs.push(Boolean(val))
+          },
+          'state.setObject': (keyPtr, valPtr) => {
+            const key = insta.exports.__getString(keyPtr)
+            const val = insta.exports.__getString(valPtr)
+            
+            IOGas = IOGas + key.length + val.length
 
     
+            wasmRunner.stateCache.set(key, val)
+            return 1
+          },
+          'state.getObject': async (keyPtr) => {
+            const key = insta.exports.__getString(keyPtr)
+            let value;
+            if(wasmRunner.stateCache.has(key)) {
+              value = wasmRunner.stateCache.get(key)
+            } else {
+              value = await stateAccess.client.pull(key)
+              wasmRunner.stateCache.set(key, value)
+            }
 
-    process.send({
-        type: 'ready'
-    })
-    
-    process.on('message', async (message: any) => {
-        
-        if(message.type === 'call') {
-            const memory = new WebAssembly.Memory({
-                initial: 10,
-                maximum: 128,
-              });
-        
-            const insta = await instantiate(binaryData, {
-                env: {
-                    memory
-                },
-                input: {
-                    consoleLog: (d) => {
-                        
-                        console.log('d', insta.exports.__getString(d))
-                        // return 44
-                    },
-                    logNumber: (d) => {
-                        console.log('logNumber', d)
-                    },
-                    logBool: (d) => {
-                        console.log('logBool', d)
-                    },
-                    
-                    logUint8Array: (d) => {
-                        console.log('logUint8Array', d)
-                    },
-                    "db.setObject": (keyPtr, valPtr) => {
-                        const key = insta.exports.__getString(keyPtr)
-                        const val = insta.exports.__getString(valPtr)
-                        
-                        console.log('setObject', key, val)
-                        return 1;
-                    },
-                    "db.getObject": (key) => {
-                        console.log('getObject', key)
-                        return "hello"
-                    },
-                    api: async () => {
-                        const { data } = await Axios.get('http://ipinfo.io/json')
-                        console.log(data)
-                        
-                        return insta.exports.__newString(data.ip)
-                    }
-                }
-            } as any)
-            console.log('message from parent:', message, new Date(), new Date().getTime());
-            const ptr = await (insta.instance.exports[message.action] as any)(insta.exports.__newString(message.payload))
-            const str = insta.exports.__getString(ptr)
-            console.log('message to parent:', message, new Date(), new Date().getTime());
-            process.send({
-                type: 'result',
-                result: str,
-                reqId: message.reqId
-            })
-        }
-    });
+            const val = JSON.stringify(value)
+            
+            IOGas = IOGas + val.length; // Total serialized length of gas
+
+
+            return val
+          },
+        },
+      } as any)
+      
+
+      
+
+
+      console.log('message from parent:', message, new Date(), new Date().getTime())
+      const ptr = await (insta.instance.exports[message.action] as any)(
+        insta.exports.__newString(message.payload),
+      )
+      const str =  insta.exports.__getString(ptr)
+      process.send({
+        type: 'execute-stop',
+        result: str,
+        logs,
+        reqId: message.reqId,
+        IOGas,
+      })
+      
+      console.log('message to parent:', message, new Date(), new Date().getTime())
+      console.log('logs', logs, 'result', str)
+      // process.send({
+      //   type: 'result',
+      //   result: str,
+      //   reqId: message.reqId,
+      // })
+    }
+    if(message.type === "finish") {
+      for(let [key, value] of wasmRunner.stateCache.entries()) {
+        await stateAccess.client.update(key, JSON.parse(value))
+      }
+      console.log(stateAccess.finish())
+      process.send({
+        type: 'result',
+        result: stateAccess.finish().stateMerkle.toString(),
+      })
+    }
+  })
 })()

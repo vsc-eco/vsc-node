38698140c53c2b7912fee0052631b199361cefef
vaultec <47548474+vaultec81@users.noreply.github.com>
2024-02-06 23:47:56 -0800

    feat: sortTransactions, computeKeyId, covnertTxJws

--- src/services/new/utils/index.ts
+++ src/services/new/utils/index.ts
@@ -47,3 +47,90 @@ export async function verifyTx(txData, did: DID) {
         }
     }
   }
+
+/**
+ * Sort transactions according to sort key.
+ */
+export function sortTransactions(rawTransactions: Array<any>, vrfSeed: string) {
+    const preOrder = ShuffleSeed.shuffle(rawTransactions.sort((a, b) => {
+        return a.id - b.id
+    }).map(e => {
+        return {
+            act: e.act
+        }
+    }), vrfSeed)
+    let dividedArray = {}
+    rawTransactions.forEach(e => {
+        if(dividedArray[e.act]) {
+            dividedArray[e.act].push(e)
+        } else {
+            dividedArray[e.act] = [e]
+        }
+    })
+    for(let key in dividedArray) {
+        dividedArray[key] = dividedArray[key].sort((a, b) => {
+            return a.nonce - b.nonce
+        })
+    }
+    const txOut = []
+    for(let tx of preOrder) {
+        txOut.push(dividedArray[tx.act].shift())
+    }
+    return txOut;
+}
+
+
+/**
+ * Computes key ID of multiple keys, or passes through single key if not.
+ */
+export async function computeKeyId(keyGroup: Array<string>) {
+    if(keyGroup.length === 1) {
+        return keyGroup[0]
+    } else {
+        const objMap = {}
+        keyGroup.forEach(e => {
+            objMap[e] = null
+        })
+        
+        return (await encodePayload(objMap)).cid.toString()
+    }
+}
+
+/**
+ * Converts TX format to dag JWS for verification
+ */
+export function convertTxJws(args: {
+    sig: string
+    tx: string
+}) {
+    const tx = Buffer.from(args.tx, 'base64url')
+    const sig = DagCbor.util.deserialize(Buffer.from(args.sig, 'base64url')) as {
+        __t: 'vsc-sig',
+        sigs: [
+            {
+                alg: string
+                kid: string
+                sig: Buffer
+            }
+        ]
+    }
+
+    
+
+
+    let jwsDag = {
+        jws: {
+            signatures: [
+                {
+                    protected: JSON.stringify({
+                        alg: sig.sigs[0].alg,
+                        kid: [sig.sigs[0].kid, sig.sigs[0].kid].join('#')
+                    }),
+                    signature: Buffer.from(sig.sigs[0].sig).toString('base64url')
+                }
+            ]
+        },
+        linkedBlock: tx
+    }
+    return jwsDag
+}

704fc26304dd785a5ff61bef11ae99dec73003f5
vaultec <47548474+vaultec81@users.noreply.github.com>
2024-04-23 20:29:51 -0700

    fix: remove test authority from custody account

--- src/services/new/witness/multisig.ts
+++ src/services/new/witness/multisig.ts
@@ -153,7 +153,8 @@ export class MultisigSystem {
                 account: networks[this.self.config.get('network.id')].multisigAccount,
                 owner: {
                     //Backup account for now. It will be removed in future versions
-                    account_auths: [['vsc.network', multisigConf.threshold]],
+                    // account_auths: [['vsc.network', multisigConf.threshold]],
+                    account_auths: [],
                     key_auths: orderAlphabetically(ownerKeys.map(e => [e, 1])),
                     
                     weight_threshold: multisigConf.threshold

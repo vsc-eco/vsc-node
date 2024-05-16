800010c6af0a68280285a9e4afc0ffe1c4fa2cf5
Geoffrey Casper <gcasper42@gmail.com>
2024-05-16 07:13:18 -0400

    remove nodejs stuff

--- src/scripts/config.ts
+++
@@ -1,4 +0,0 @@
-
-export const globalConfig = {
-    btcTokenContract: '59dfb8383291734049bfab403ced85a57cbcde6a'
-}
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install --legacy-peer-deps &&
-if [ "$DEV" == "true" ]; then
-  npm run dev
-else
-  npm run build &&
-  npm run start
-fi
